a87b16519b647af4011e2e66d8729636ad6b7b8d
alexvest <78535319+alexvest@users.noreply.github.com>
2023-11-28 13:51:26 +0100

    Inför F7

---
+++ lectures/L7/slutkod/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="sv">
+    <head>
+        <meta charset="utf-8">
+        <title>PHP F7</title>
+    </head>
+    <body>
+        <main>
+    
+        </main>
+    </body>
+</html>
