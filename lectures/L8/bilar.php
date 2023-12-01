c0e88d0ec741212edf23f93b81ff33ee03a6a4f6
peteratkau <59958667+peteratkau@users.noreply.github.com>
2023-12-01 08:58:12 +0100

    Create bilar.php

---
+++ lectures/L8/bilar.php
@@ -0,0 +1,28 @@
+<?php
+    include("databaseFunctions.php");
+?>
+<!DOCTYPE html>
+<html lang="sv">
+    <head>
+        <meta charset="utf-8">
+        <title>PHP F8</title>
+    </head>
+    <body>
+        <main>
+        
+        <?php
+            try {
+                
+                
+            }catch(PDOException $e) {
+                
+            
+            } finally{
+            
+            }
+            
+        ?>
+
+        </main>
+    </body>
+</html>
