33688bc95d2c1bab51f4df20e58057acdd0071fb
peteratkau <59958667+peteratkau@users.noreply.github.com>
2023-12-04 09:28:46 +0100

    Update databaseFunctions.php

--- lectures/L8/databaseFunctions.php
+++ lectures/L8/databaseFunctions.php
@@ -1,6 +1,12 @@
 <?php
 
-    function connectToDatabase() {}
+    function connectToDatabase() {
+        
+        $dbhsOptions = array(
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        );
+    }
 
     function listCarsAsForms($dbh) {
         ?>
