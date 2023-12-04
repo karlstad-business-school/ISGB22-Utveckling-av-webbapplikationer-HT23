fc202381f6c5f494a55494f6f3ba2b1bbc1edbba
peteratkau <59958667+peteratkau@users.noreply.github.com>
2023-12-04 16:05:26 +0100

    Update bilar.php

--- lectures/L8/bilar.php
+++ lectures/L8/bilar.php
@@ -13,20 +13,32 @@
         <?php
             try {
 
-                if(isset($_POST["btnDelete"])) { //DELETE
+                $dbh = connectToDatabase();
+
+                if(isset($_POST["btnDelete"])) {
+                    //DELETE
+                    //Ropa på deleteCar() skicka med den data funktionen önskar ta emot.
                 }
 
-                if(isset($_POST["btnSave"])) { //UPDATE eller INSERT
+                if(isset($_POST["btnSave"])) {
+                    //UPDATE eller INSERT
+                    //Om hidId finns ropa på updateCar() annars insertCar() och skicka med den data funktionerna önskar ta emot.
                 }
 
-                if(isset($_POST["btnEdit"])) { //SELECT eller inte.
+                if(isset($_POST["btnEdit"])) {
+                    //SELECT
+                    selectCar($dbh, $_POST["hidId"]);
+                } else {
+                    createForm();
                 }
+
+                listCarsAsForms($dbh);
                 
             }catch(PDOException $e) {
-                
+                echo($e->getMessage());
             
             } finally{
-            
+                $dbh = null;
             }
             
         ?>
