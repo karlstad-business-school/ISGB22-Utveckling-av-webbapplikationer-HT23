8890ab560a96645bb0044957072fde1721e62b9b
Pierre Sjöberg <pierre.sjoberg@kau.se>
2023-12-12 11:36:34 +0100

    F10 slutkod

--- lectures/L10/slutkod/kodexempel.php
+++ lectures/L10/slutkod/kodexempel.php
@@ -1,7 +1,7 @@
 <?php
     function dbConnect() {
         try {
-            $dns = "mysql:dbname=test;host=localhost";
+            $dns = "mysql:dbname=fisk;host=localhost";
             $user = "root";
             $password = "";
             $options = array( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
@@ -32,6 +32,86 @@
         </header>
 
         <main>    
+
+            <?php 
+
+                if(isset($_POST["skicka"])){
+
+                    //Vettig validering (Sparas till workshop)
+                    //1. Kolla dublett av epost i databasen
+                    //2. Kolla att eposten är giltig
+                    //3. Kolla losenord antal tecken + siffra + specicialtecken
+                    //4. Kolla att båda lösen är samma
+
+                    //Validera personnummer
+
+                    try {
+                        $dbh = dbConnect();
+
+                        $sql = "INSERT INTO users(epost, losenord, personnummer) VALUES(:epost,:losen,:fisk);";
+
+                        $stmt = $dbh->prepare($sql);
+
+                        $losen = $_POST["losen1"];
+                        $hashatLosen = hash("SHA256",$losen);
+
+                        $persnr = $_POST["persnr"];
+                        $nyckel = "valfristrang1234";
+                        $metod = "AES-128-ECB";
+                        $krypteratPersonnummer = openssl_encrypt($persnr, $metod, $nyckel);
+
+                        $stmt->bindValue(":epost", $_POST["epost"]);
+                        $stmt->bindValue(":losen", $hashatLosen);
+                        $stmt->bindValue(":fisk", $krypteratPersonnummer);
+
+                        $stmt->execute();
+
+                        $stmt = null;
+                        $dbh = null;
+
+
+                    }
+                    catch(Exception $ex) {
+                        //Gör nåt kul
+                    }
+
+
+
+
+
+
+                }
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            ?>
+
+
+            
+        <form method="POST" action="<?php echo($_SERVER["PHP_SELF"]) ?>">
+            <div class="form-group">
+                <label for="epost">Ange din epost:</label>
+                <input type="email" name="epost" id="epost" required>
+            </div>
+            <div class="form-group">
+                <label for="persnr">Ange personnummer (YYMMDD-XXXX)</label>
+                <input type="text" id="persnr" name="persnr" required>
+            </div>
+            <div class="form-group">
+                <label for="losen1">Välj ett lösenord (Upprepa två gånger)</label>
+                <input type="password" id="losen1" name="losen1">
+                <input type="password" id="losen2" name="losen2">
+            </div>
+            <input type="submit" name="skicka" class="btn btn-danger" value="Skicka">
+        </form>
+            
 
       
             
